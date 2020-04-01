66d254477924a4188569843644dd8e97235ca89f
geweixinerr <geweixinerr@vip.163.com>
2020-04-01 15:59:51 +0800

    解决重复执行问题

--- micro-web/src/main/java/micro/web/config/shiro/ShiroConfig.java
+++ micro-web/src/main/java/micro/web/config/shiro/ShiroConfig.java
@@ -12,7 +12,6 @@ import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSource
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
-import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -111,17 +110,5 @@ public class ShiroConfig {
 		AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
 		authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
 		return authorizationAttributeSourceAdvisor;
-	}
-
-	/**
-	 * 开启shiro注解通知器
-	 * 
-	 * @author gewx
-	 */
-	@Bean
-	public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
-		DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
-		advisorAutoProxyCreator.setProxyTargetClass(true);
-		return advisorAutoProxyCreator;
 	}
 }
