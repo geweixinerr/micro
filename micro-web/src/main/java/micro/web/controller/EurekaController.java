0e822e1280382ec631296d9038a519ef485636b1
geweixin <geweixinerr@vip.163.com>
2020-04-04 22:07:09 +0800

    代码fix

--- micro-web/src/main/java/micro/web/controller/EurekaController.java
+++ micro-web/src/main/java/micro/web/controller/EurekaController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import micro.service.demo.DemoService;
 import micro.service.demo.FeignServiceImpl;
 import micro.web.util.Response;
 
@@ -23,13 +22,9 @@ public class EurekaController {
 	@Autowired
 	private FeignServiceImpl feignClient;
 	
-	@Autowired
-	private DemoService demoService;
-	
 	@RequestMapping(value = "/feignCall", method = RequestMethod.GET)
 	public Map<String, Object> login() {
-		//feignClient.callFeign();
-		demoService.syncDataById(1024);
+		feignClient.callFeign();
 		return Response.SUCCESS.newBuilder().toResult();
 	}
 }
