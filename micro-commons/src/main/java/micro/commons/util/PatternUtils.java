f8b3dd80ff5093aa551bc11fe7a957bb4b038953
geweixin <geweixinerr@vip.163.com>
2020-05-28 14:17:59 +0800

    正则表达式工具类

---
+++ micro-commons/src/main/java/micro/commons/util/PatternUtils.java
@@ -0,0 +1,19 @@
+package micro.commons.util;
+
+import java.util.regex.Pattern;
+
+import micro.commons.annotation.ThreadSafe;
+
+/**
+ * 正则表达式工具类,预编译了一批可复用表达式
+ * 
+ * @author gewx
+ * **/
+@ThreadSafe
+public final class PatternUtils {
+
+	/**
+	 * 正则表达式,支持整数/小数(精度到后两位)
+	 * **/
+	public static final Pattern NUMERIC_REGEX = Pattern.compile("^([1-9]\\d*(\\.\\d{1,2})?|0\\.\\d{1,2}|0)$");  
+}
