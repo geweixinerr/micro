ea9993c5219fd409dbe5df4a9c9e31a1d3173073
geweixin <geweixinerr@vip.163.com>
2021-02-22 19:34:58 +0800

    Fix:恢复

--- micro-commons/src/main/java/micro/commons/task/AbstractTaskBeanDelayed.java
+++ micro-commons/src/main/java/micro/commons/task/AbstractTaskBeanDelayed.java
@@ -3,15 +3,12 @@ package micro.commons.task;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-
 /**
  * 任务延迟任务队列
  * 
  * @author gewx
  **/
-public final class TaskBeanDelayed implements Delayed {
+public abstract class AbstractTaskBeanDelayed implements Delayed, Runnable {
 
 	/**
 	 * 任务Id
@@ -27,16 +24,6 @@ public final class TaskBeanDelayed implements Delayed {
 	 * 任务
 	 **/
 	private Runnable task;
-
-	/**
-	 * 重试-最大次数
-	 **/
-	private int retryMax;
-
-	/**
-	 * 重试当前次数
-	 **/
-	private int retryNum;
 
 	/**
 	 * 延迟时间
@@ -83,55 +70,11 @@ public final class TaskBeanDelayed implements Delayed {
 		this.task = task;
 	}
 
-	public int getRetryMax() {
-		return retryMax;
-	}
-
-	public void setRetryMax(int retryMax) {
-		this.retryMax = retryMax;
-	}
-
-	public int getRetryNum() {
-		return retryNum;
-	}
-
-	public void setRetryNum(int retryNum) {
-		this.retryNum = retryNum;
-	}
-
 	public long getExpire() {
 		return expire;
 	}
 
 	public void setExpire(long expire) {
 		this.expire = expire;
-	}
-
-	@Override
-	public int hashCode() {
-		HashCodeBuilder builder = new HashCodeBuilder();
-		builder.append(this.taskId);
-		return builder.toHashCode();
-	}
-
-	@Override
-	public boolean equals(Object otherObject) {
-		if (otherObject == null) {
-			return false;
-		}
-
-		if (this == otherObject) {
-			return true;
-		}
-
-		if (!(otherObject instanceof TaskBeanDelayed)) {
-			return false;
-		}
-
-		TaskBeanDelayed taskObject = (TaskBeanDelayed) otherObject;
-
-		EqualsBuilder builder = new EqualsBuilder();
-		builder.append(taskObject.taskId, this.taskId);
-		return builder.isEquals();
 	}
 }
